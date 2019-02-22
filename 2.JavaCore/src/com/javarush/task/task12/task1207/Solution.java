95fdb231dd52b8aa9e1b8831c3c610c456c188fa
pavelkor91 <pavelkor91@gmail.com>
2019-02-22 11:20:31 +0300

    level 12 - abstract class

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(5);
+        print((Integer) 5);
+    }
+    public static void print (int i){
+
+    }
+    public static void print (Integer i){
+
+    }
+    //Напишите тут ваши методы
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static void print (int i){
+
+    }
+    public static void print (Integer i){
+
+    }
+    public static void print (String s){
+
+    }
+    public static void print (int i, String s){
+
+    }
+    public static void print (int i, double d){
+
+    }
+    //Напишите тут ваши методы
+}
