2e79bf6916f3e1e3793de67aa53838e7b7557431
pavelkor91 <pavelkor91@gmail.com>
2019-02-26 18:42:10 +0300

    level 14

--- 2.JavaCore/src/com/javarush/task/task14/task1404/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1404/Solution.java
@@ -4,9 +4,25 @@ package com.javarush.task.task14.task1404;
 Коты
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Cat> cats = new ArrayList<>();
+        while (true){
+            String txt = bufferedReader.readLine();
+            if(txt.equals(""))
+                break;
+            else
+                cats.add(CatFactory.getCatByKey(txt));
+        }
+
+        for(Cat cat: cats){
+            System.out.println(cat.toString());
+        }
     }
 
     static class CatFactory {
