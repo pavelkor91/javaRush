bb0c0c1bb7614336ca918019afd421855061b29e
pavelkor91 <pavelkor91@gmail.com>
2019-04-02 22:59:02 +0300

    level 19 - adapters

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task19.task1902;
+
+/* 
+Адаптер
+*/
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AdapterFileOutputStream implements AmigoStringWriter{
+
+    private FileOutputStream fileOutputStream;
+
+    public AdapterFileOutputStream(FileOutputStream fileOutputStream){
+        this.fileOutputStream = fileOutputStream;
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.fileOutputStream.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        this.fileOutputStream.flush();
+    }
+
+    @Override
+    public void writeString(String s) throws IOException {
+        this.fileOutputStream.write(s.getBytes());
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -0,0 +1,77 @@
+package com.javarush.task.task19.task1903;
+
+/* 
+Адаптация нескольких интерфейсов
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    static {
+        countries.put("UA","Ukraine");
+        countries.put("RU","Russia");
+        countries.put("CA","Canada");
+    }
+    public static void main(String[] args) {
+
+    }
+
+    public static class IncomeDataAdapter implements Customer, Contact{
+        private IncomeData data;
+
+        public IncomeDataAdapter(IncomeData data){
+            this.data = data;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+
+        @Override
+        public String getName() {
+            return data.getContactLastName() + ", " + data.getContactFirstName();
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            String prolonged = String.format("%010d", data.getPhoneNumber());
+            return String.format("+%d(%3s)%3s-%2s-%2s",data.getCountryPhoneCode(), prolonged.substring(0, 3), prolonged.substring(3, 6), prolonged.substring(6, 8), prolonged.substring(8, 10));
+        }
+    }
+
+
+    public static interface IncomeData {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        int getCountryPhoneCode();      //For example: 38
+
+        int getPhoneNumber();           //For example: 501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example: +38(050)123-45-67
+    }
+}
