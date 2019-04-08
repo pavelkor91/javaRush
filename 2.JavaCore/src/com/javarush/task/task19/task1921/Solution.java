9fe7ddde3e78646a17f4ce6e54ec60f52c53c963
pavelkor91 <pavelkor91@gmail.com>
2019-04-08 11:18:33 +0300

    level 19 - last tasks

--- 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -3,10 +3,9 @@ package com.javarush.task.task19.task1921;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /* 
 Хуан Хуанович
@@ -24,6 +23,27 @@ public class Solution {
         while (bufferedReader.ready()) {
             String[] pair = bufferedReader.readLine().split(" ");
 
+            StringBuilder date = new StringBuilder();
+            date.append(pair[pair.length - 3]); date.append(" ");
+            date.append(pair[pair.length - 2]); date.append(" ");
+            date.append(pair[pair.length - 1]); date.append(" ");
+
+            Date personDate = null;
+            try {
+                personDate = new SimpleDateFormat("dd MM yyyy").parse(date.toString());
+            }
+            catch (ParseException e){}
+
+            StringBuilder name = new StringBuilder();
+            for (int i = 0; i < pair.length - 3; i++) {
+                name.append(pair[i]);
+                name.append(" ");
+            }
+            PEOPLE.add(new Person(name.toString().trim(), personDate));
         }
+        fileReader.close();
+
+        for(Person person: PEOPLE)
+            System.out.println(person.getName() + " " + person.getBirthDate().toString());
     }
 }
