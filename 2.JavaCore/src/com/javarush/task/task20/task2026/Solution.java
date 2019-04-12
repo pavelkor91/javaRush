d903726690aad7b2cc352892ff44458b0734dc23
pavelkor91 <pavelkor91@gmail.com>
2019-04-12 13:04:53 +0300

    level 20 - rectangles

--- 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -25,6 +25,34 @@ public class Solution {
     }
 
     public static int getRectangleCount(byte[][] a) {
-        return 0;
+        int count = 0;
+        int corners = 0;
+        int lines = 0;
+        int dots = 0;
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a.length; j++) {
+                int sum = 0;
+                if (a[i][j] == 1) {
+                    for (int n = i - 1; n <= i + 1; n++) {
+                        for (int m = j - 1; m <= j + 1; m++) {
+                            try {
+                                sum += a[n][m];
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                continue;
+                            }
+                        }
+                    }
+                    sum = sum - a[i][j];
+                    if (sum == 3)
+                        corners++;
+                    if (sum == 1)
+                        lines++;
+                    if (sum == 0)
+                        dots++;
+                }
+            }
+        }
+        count = corners / 4 + lines / 2 + dots;
+        return count;
     }
 }
