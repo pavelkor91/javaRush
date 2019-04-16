1842889b2d49ecd1ebe4f218ac43dc00048723d3
pavelkor91 <pavelkor91@gmail.com>
2019-04-16 21:32:26 +0300

    level 21 - NIO

--- 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
@@ -1,12 +1,24 @@
 package com.javarush.task.task31.task3107;
 
-/* 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/*
 Null Object Pattern
 */
 public class Solution {
     private FileData fileData;
 
     public Solution(String pathToFile) {
+        try {
+            Path path = Paths.get(pathToFile);
+            this.fileData = new ConcreteFileData(Files.isHidden(path), Files.isExecutable(path), Files.isDirectory(path), Files.isWritable(path));
+        }
+        catch (Exception e){
+            this.fileData = new NullFileData(e);
+        }
     }
 
     public FileData getFileData() {
