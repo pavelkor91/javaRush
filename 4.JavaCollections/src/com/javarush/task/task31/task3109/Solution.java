7b34755ab56a48e67d94c00a6d9c19970d6258e6
pavelkor91 <pavelkor91@gmail.com>
2019-04-16 21:51:21 +0300

    level 21 - properties

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) throws IOException {
+        File file = new File(fileName);
+        Properties properties = new Properties();
+        if(file.exists()){
+            if(fileName.endsWith(".xml")){
+                properties.loadFromXML(new FileInputStream(file));
+            }
+            else {
+                properties.load(new FileReader(file));
+            }
+        }
+        return properties;
+    }
+}
