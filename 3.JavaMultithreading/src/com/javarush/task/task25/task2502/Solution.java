b4ea4eadc5dc11ea3fb2473fe116cae2ff2ac3bd
pavelkor91 <pavelkor91@gmail.com>
2019-04-26 17:39:18 +0300

    level 24 - Enum

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() throws Exception{
+
+            wheels = new ArrayList<>();
+            int wheelCount = 0;
+            for (String wheel : loadWheelNamesFromDB()){
+                wheels.add(Wheel.valueOf(wheel));
+                wheelCount += 1;
+            }
+            if (wheelCount != 4)
+                throw new Exception();
+        }
+
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
