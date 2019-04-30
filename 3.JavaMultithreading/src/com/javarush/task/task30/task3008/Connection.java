d02e4dbc55aa334bd364a831f43482e0d382b53f
pavelkorchagin <pavelkor91@gmail.com>
2019-04-30 23:59:45 +0300

    Level 26 = chat 10

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 public class Connection implements Closeable {
 
@@ -18,11 +19,20 @@ public class Connection implements Closeable {
         in = new ObjectInputStream(socket.getInputStream());
     }
     public void send(Message message) throws IOException{
-
+        synchronized (out) {
+            out.writeObject(message);
+        }
     }
 
     public Message receive() throws IOException, ClassNotFoundException{
-        return null;
+        synchronized (in) {
+            return (Message) in.readObject();
+        }
+    }
+
+
+    public SocketAddress getRemoteSocketAddress(){
+        return socket.getRemoteSocketAddress();
     }
 
     @Override
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,83 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new java.util.concurrent.ConcurrentHashMap<>();
+
+    private static class Handler extends Thread{
+        private Socket socket;
+        public Handler (Socket socket){
+            this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException{
+            Message message;
+            while (true){
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                message = connection.receive();
+                if(message.getType() == MessageType.USER_NAME){
+                    if(message.getData() != null && !message.getData().equals(""))
+                        if(connectionMap.get(message.getData()) == null)
+                            break;
+                }
+            }
+            connectionMap.put(message.getData(), connection);
+            connection.send(new Message(MessageType.NAME_ACCEPTED));
+            return message.getData();
+        }
+
+        private void notifyUsers(Connection connection, String userName) throws IOException{
+            for(Map.Entry<String, Connection> entry : connectionMap.entrySet()){
+                if(userName.equals(entry.getKey()))
+                    continue;
+                else
+                    connection.send(new Message(MessageType.USER_ADDED, entry.getKey()));
+            }
+        }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException{
+            while (true){
+                Message message = connection.receive();
+                if(message.getType() == MessageType.TEXT){
+                    sendBroadcastMessage(new Message(MessageType.TEXT, userName + ": " + message.getData()));
+                }
+                else
+                    ConsoleHelper.writeMessage("Ошибка - не текст.");
+            }
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        for(Map.Entry<String, Connection> entry: connectionMap.entrySet()){
+            try {
+                entry.getValue().send(message);
+            }
+            catch (IOException e){
+                ConsoleHelper.writeMessage(e.toString());
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Сервер запущен!");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        }
+        catch (IOException e) {
+            ConsoleHelper.writeMessage("Error");
+        }
     }
 }
+
