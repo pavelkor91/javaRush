65b7b4c42b9a96ad81ad6ceb0b5ce4ca2f0fcc0f
pavelkor91 <pavelkor91@gmail.com>
2019-05-02 00:11:35 +0300

    level 27 - deadlock

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task27.task2710;
+
+public class MailServer implements Runnable {
+    private Mail mail;
+
+    public MailServer(Mail mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void run(){
+        long startTime = System.currentTimeMillis();
+
+            while (mail.getText() == null) {
+                synchronized (mail) {
+                    try {
+                        mail.wait();
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+            String name = Thread.currentThread().getName();
+            long endTime = System.currentTimeMillis();
+            System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
+    }
+}
