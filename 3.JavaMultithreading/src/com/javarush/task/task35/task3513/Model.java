ece06c9aad3c6de4041b0e46b950d3c7c816f39c
pavelkor91 <pavelkor91@gmail.com>
2019-05-08 16:28:37 +0300

    lvl 30 - 2048

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,16 +1,50 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
+    private int score;
+    private int maxTile;
+
 
     public Model(){
-        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        resetGameTiles();
+        score = 0;
+        maxTile = 0;
+    }
+
+    private List<Tile> getEmptyTiles(){
+        List<Tile> list = new ArrayList<>();
         for(int i = 0; i < FIELD_WIDTH; i++){
             for(int j = 0; j < FIELD_WIDTH; j++){
+                if(gameTiles[i][j].value == 0){
+                    list.add(gameTiles[i][j]);
+                }
+            }
+        }
+        return list;
+    }
+
+    private void addTile(){
+        List<Tile> list = getEmptyTiles();
+        if (list != null && list.size() != 0) {
+            list.get((int) (list.size() * Math.random() )).setValue(Math.random() < 0.9 ? 2 : 4);
+        }
+    }
+
+    protected void resetGameTiles(){
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for(int i = 0; i< FIELD_WIDTH ; i++){
+            for (int j = 0; j < FIELD_WIDTH ; j++){
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile();
+        addTile();
     }
+
 
 }
--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Tile.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Tile.java
@@ -17,6 +17,10 @@ public class Tile {
         return value == 0;
     }
 
+    public void setValue(int value) {
+        this.value = value;
+    }
+
     public Color getFontColor(){
         return value < 16 ? new Color(0x776e65) : new Color(0xf9f6f2);
     }
