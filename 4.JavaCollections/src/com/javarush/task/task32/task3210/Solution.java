382b28e5bb1b19035ed230252b9df843a2c44224
pavelkor91 <pavelkor91@gmail.com>
2019-05-12 15:07:35 +0300

    level 33 - JSON

--- 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -23,6 +23,5 @@ public class Solution {
            raf.write("false".getBytes());
        }
        raf.close();
-
     }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task33.task3303;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+
+/* 
+Десериализация JSON объекта
+*/
+public class Solution {
+    public static <T> T convertFromJsonToNormal(String fileName, Class<T> clazz) throws IOException {
+       T result = null;
+        ObjectMapper mapper = new ObjectMapper();
+        StringReader stringReader;
+
+        result = mapper.readValue(new FileReader(new File(fileName)), clazz);
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
